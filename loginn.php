f1e920dcb6130e5318c6b6ba5535ed997a6e85b4
Tanjil <tanjilkh@gmail.com>
2023-10-25 20:23:48 +0100

    current_page_item

--- loginn.php
+++ loginn.php
@@ -7,9 +7,25 @@
     <title>Login</title>
 </head>
 <body>
-<?php
-include("header.php")
-?>
+<header>
+        <div id="logo"> <a href="index.php"><img src="./image/PK_white.png" alt="" width="100"></a>
+            
+
+           
+        </div>
+        <div id="menu">
+            <nav>
+                <ul class="nav-list">
+                    <li >
+                        <a href="index.php">Página Inicial</a>
+                    </li>
+                    <li class="first current_page_item">
+                        <a href="loginn.php">Login</a>
+                    </li>
+                   
+                </ul>
+            </nav>
+    </header>
     
     <div class="login-box">
         <p>Login</p>
